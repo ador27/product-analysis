c80cb12baeb5931f1ee2c8e7f7a3ef6ab54483a5
ador <rashedulhaque111@gmail.com>
2022-04-04 05:48:44 +0600

    home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Welcome TO AFFINITY</h1>
+            <div className="details">
+                <h1>WELCOME TO AFFINITY</h1>
+                <h1>SEE THE BEST HOODIE IN TOWN</h1>
+
+            </div>
+            <div className="hoodie">
+
+            </div>
         </div>
     );
 };
