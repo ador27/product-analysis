787881443eff58f94e709c6459c9761546033bc1
ador <rashedulhaque111@gmail.com>
2022-04-05 02:11:54 +0600

    notfound page

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -21,12 +21,7 @@ const NotFound = () => {
                     </Link>
                 </div>
                 <div>
-                    <div className='images'>
-                        <img
-                            src='https://thumbs.dreamstime.com/b/%C3%B0%C2%BF%C3%B0%C2%B5%C3%B1%E2%80%A1%C3%B0%C2%B0%C3%B1%E2%80%9A%C3%B1%C5%93-134036857.jpg'
-                            alt=''
-                        />
-                    </div>
+
                 </div>
             </div>
         </section>
